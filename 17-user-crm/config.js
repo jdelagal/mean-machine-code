3977b78254c69b7e8189b5da51fde9dab96493f2
jdelagal <jdelagal@gmail.com>
2015-08-17 17:44:25 +0200

    Boton Volver y quitar columna id de la tabla de usuarios

--- 17-user-crm/config.js
+++ 17-user-crm/config.js
@@ -1,5 +1,6 @@
 module.exports = {
 	'port': process.env.PORT || 8080,
-	'database': 'mongodb://node:noder@novus.modulusmongo.net:27017/Iganiq8o',
+	//'database': 'mongodb://node:noder@novus.modulusmongo.net:27017/Iganiq8o',
+	'database': 'mongodb://localhost/Iganiq8o',
 	'secret': 'ilovescotchscotchyscotchscotch'
 };

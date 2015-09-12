40a098d82e77a6e3a15712737f48e85dd062d112
jdelagal <jdelagal@gmail.com>
2015-09-12 14:06:34 +0200

    referencia modelo catalogo

--- 17-user-crm/app/routes/api.js
+++ 17-user-crm/app/routes/api.js
@@ -1,6 +1,5 @@
 var bodyParser = require('body-parser'); 	// get body-parser
 var User       = require('../models/user');
-var Catalogo       = require('../models/catalogo');
 var jwt        = require('jsonwebtoken');
 var config     = require('../../config');
 
